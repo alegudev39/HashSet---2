3613680934b920a7e401e358075aea5e9a73c746
AleDev <aledev@example.com>
2024-03-07 18:14:04 +0100

    prova

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.HashSet;
 
+
+//commento nuova pull branch addNewFeature
+
 //Scrivere una funzione che restituisca un HashSet riempito
 //Creare un oggetto dello stesso tipo inserito nell'HashSet e popolarlo
 //Verificare che l' elemento sia parte del Set e stampare il risultato
